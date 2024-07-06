90829359536bc0a004b110c95de17571f3bee71f
Nishant <nissshhdev@gmail.com>
2024-07-06 16:58:59 +0530

    fixed hero on dektop

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="flex justify-between items-center p-4 bg-white border border-gray-600 border-b-1">
       <div className="flex flex-col  justify-center">
-        <h2 className="text-xl font-light ">HELLO, I'M</h2>
+        <h2 className="text-4xl font-light ">HELLO, I'M</h2>
         <h1 className="text-4xl font-bold  mb-2">
           Nishant<br />
           Kumar
@@ -13,11 +13,11 @@ const Header = () => {
         <p className="text-sm">Lovely Professional University , India</p>
       </div>
       <div className="relative w-1/2">
-        <div className="absolute  top-0 left-0 w-24 h-24 opacity-30 bg-red-500 rounded-full"></div>
+        <div className="absolute  top-0 left-0 w-24 h-24 opacity-30 bg-red-500 rounded-full md:w-24 md:h-24"></div>
         <img 
           src="https://i.postimg.cc/Qd9B9Fn9/pic.png" 
           alt="nishant" 
-          className="w-full h-auto grayscale "
+          className="w-48 h-48 grayscale "
         />
       </div>
     </div>
