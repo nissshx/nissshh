1e88e0e3e4d50a4e0d358e7d084d4db274b1a17d
Nishant <nissshhdev@gmail.com>
2024-07-07 17:56:06 +0530

    added analytics

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
+import { Analytics } from "@vercel/analytics/react"
 
 
 import Header from './components/Header';
